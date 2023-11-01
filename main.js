665ceb1367f106c684c8e2a96a2a55d11de9061a
geraldinho12 <130573202+geraldinho12@users.noreply.github.com>
2023-11-01 10:27:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function tocaSom(seletorAudio){  
+const elemento = document.querySelector(seletorAudio).play('');
+}       
+
+
+if(elemento != null && elemento.localName === 'audio'){
+    elemento.play();
+}else{
+    alert('Elemento não encontrado');
+}
+const lista = document.querySelectorAll('.tecla')    
+
+for (contador = 0; contador < lista.legnth;){}
+
+while(contador < 9, lista.legnth){
+    lista [contador].classList[1];
+
+    const instrumento = lista[contador];
+    const tecla = lista[contador];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla[contador].onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){
+        if(evento.code=='Space' || evento.code ==='Enter'){
+        tecla.classList.add('ativa');}
+    }
+    }
